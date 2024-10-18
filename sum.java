04acf99c914a99aa1229eaa05b00e629aa5715a6
Sohail Sheikh <ssohailsheikh92@gmail.com>
2024-10-18 20:49:03 +0530

    Initial Commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        System.out.println("Sohail");
+        int a=8;
+        int b=9;
+        System.out.println(a+b);
+    }
+}
