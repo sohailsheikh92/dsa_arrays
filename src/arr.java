f44b26f502f3b24d25d90de8f9396406adb74f30
Sohail Sheikh <ssohailsheikh92@gmail.com>
2024-10-19 08:40:00 +0530

    print

--- src/arr.java
+++ src/arr.java
@@ -1,5 +1,5 @@
 public class arr {
     public static void main(String[] args) {
-
+        System.out.println("alhamdulillah learned github");
     }
 }
--- src/array.java
+++ src/array.java
@@ -1,2 +1,5 @@
 public class array {
+    public static void main(String[] args) {
+
+    }
 }
