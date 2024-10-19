45c458ee831c39b1cb3d3bbf521585a6ac51b840
Sohail Sheikh <ssohailsheikh92@gmail.com>
2024-10-19 14:39:40 +0530

    moveZeroToEnd

--- src/arr.java
+++ src/arr.java
@@ -13,9 +13,29 @@ public class arr {
         }
         return secMax;
     }
+
+    static void moveZerosToEnd(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                for(int j=i+1;j<n;j++){
+                    if(arr[j]!=0){
+                        int temp=arr[i];
+                        arr[i]=arr[j];
+                        arr[j]=temp;
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 //        System.out.println("alhamdulillah learned github");
-        int arr[] = {1,1,1};
-        System.out.println(secLargest(arr));
+        int arr[] = {1,0,6,8,0,4,8,6,0};
+        moveZerosToEnd(arr);
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+//        System.out.println(secLargest(arr));
     }
 }
