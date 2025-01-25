ca5042a5ce8a684211548984bea6b645adc42fa4
Sohail Sheikh <ssohailsheikh92@gmail.com>
2025-01-25 15:51:03 +0530

    added abstract

--- src/Learningabstract.java
+++ src/Learningabstract.java
@@ -1,5 +1,18 @@
+abstract class generalBank{
+    public abstract void savings();
+}
+
+class savingss extends generalBank{
+
+    @Override
+    public void savings() {
+        System.out.println("Created abstract class");
+    }
+}
+
 public class Learningabstract {
     public static void main(String[] args) {
-        System.out.println("Alhamdulillah");
+            savingss obj = new savingss();
+            obj.savings();
     }
 }
